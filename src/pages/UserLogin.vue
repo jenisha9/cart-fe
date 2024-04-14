95226da0c66a1a3806534a41e289c301ec5d95dc
jenisha9 <njenisha78@gmail.com>
2024-04-14 13:34:02 +0545

    feat: add login script

--- src/pages/UserLogin.vue
+++ src/pages/UserLogin.vue
@@ -1,42 +1,29 @@
 <template>
-    <div class="flex justify-center items-center h-screen bg-gray-100">
-      <div class="bg-white p-8 rounded shadow-md w-96">
-        <h2 class="text-2xl font-semibold mb-4">Login</h2>
-        <form @submit.prevent="login">
-          <div class="mb-4">
-            <label for="email" class="block text-gray-700 font-semibold mb-2">Email:</label>
-            <input type="email" id="email" v-model="email" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500">
-          </div>
-          <div class="mb-6">
-            <label for="password" class="block text-gray-700 font-semibold mb-2">Password:</label>
-            <input type="password" id="password" v-model="password" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500">
-          </div>
-          <button type="submit" class="w-full bg-pink-300 text-white font-semibold px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Login</button>
-        </form>
+  <div class="flex justify-center items-center h-screen bg-gray-100">
+    <div class="bg-white p-8 rounded shadow-md w-96">
+      <h2 class="text-pink-500 text-2xl font-semibold mb-4">Login</h2>
+      <form @submit.prevent="submitForm" method="post">
+        <div class="mb-4">
+          <label for="email" class="block text-gray-700 font-semibold mb-2">Email:</label>
+          <input type="email" id="email" v-model="email" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500">
+        </div>
+        <div class="mb-6">
+          <label for="password" class="block text-gray-700 font-semibold mb-2">Password:</label>
+          <input type="password" id="password" v-model="password" class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500">
+        </div>
+        <button type="submit" class="w-full bg-pink-500 text-white font-semibold px-4 py-2 rounded hover:bg-pink-600 focus:outline-none focus:bg-pink-600">Login</button>
+      </form>
+      <div class="mt-4 text-center">
+        <a href='/user/register' class="text-pink-500 hover:text-pink-700 font-semibold">Create an Account</a>
       </div>
     </div>
-  </template>
+  </div>
+</template>
+
+<script setup>
+import { LoginForm } from '@/composables/LoginForm.js';
+const { email, password, submitForm} = LoginForm();
+
+</script>
   
-  <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        password: ''
-      };
-    },
-    methods: {
-      login() {
-        console.log('Login clicked');
-        console.log('Email:', this.email);
-        console.log('Password:', this.password);
-  
-      }
-    }
-  };
-  </script>
-  
-  <style>
-  /* Additional styles specific to this component can be added here */
-  </style>
   
