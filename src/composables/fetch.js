de5850f232aedee25698734df9350f03961ee56a
jenisha9 <njenisha78@gmail.com>
2024-04-18 12:34:20 +0545

    fix: handle error

--- src/composables/fetch.js
+++ src/composables/fetch.js
@@ -1,10 +1,13 @@
 
-export function useFetch(url, options) {
+  export function useFetch(url, options) {
     return new Promise((resolve, reject) => {
       fetch(url, options)
-        .then((response) => response.json())
-        .then((data) => {
-          resolve(data)
+        .then((response) => {
+          response.json().then((data) => {
+            if (response.ok) {
+              resolve(data);
+            } else reject(data);
+          })
         })
         .catch((error) => {
           reject(error)
