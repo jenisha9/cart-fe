8f35e27aaf6e6f98a74ff217363822ca23b0db23
jenisha9 <njenisha78@gmail.com>
2024-04-21 14:44:43 +0545

    fix: fix use of prop and emit

--- src/components/InputBox.vue
+++ src/components/InputBox.vue
@@ -1,32 +1,28 @@
 <template>
-    <input :type="type" v-model="inputValue" :class="inputClass" @input="updateValue">
-  </template>
-  
-  <script setup>
-  import { ref, defineProps, defineEmits } from 'vue';
-  
-  const props = defineProps({
-    type: {
-      type: String,
-      default: 'text'
-    },
-    value: {
-      type: [String, Number],
-      default: ''
-    },
-    inputClass: {
-      type: String,
-      default: 'w-full px-3 py-2 border  border-gray-300 rounded focus:outline-none focus:border-blue-500' 
-    }
-  });
-  
-  const emit = defineEmits(['input']);
-  
-  const inputValue = ref(props.value);
-  
-  const updateValue = (event) => {
-    inputValue.value = event.target.value;
-    emit('input', inputValue.value);
-  };
-  </script>
-  
+  <div>
+    <label class="block mb-2 text-left text-gray-700 font-bold">{{
+      label
+    }}</label>
+    <input
+  :value="modelValue"
+  @input="$emit('update:modelValue', $event.target.value)"
+  v-bind="$attrs"
+  class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+/>
+  </div>
+</template>
+
+<script setup>
+import defineProps from "vue";
+
+defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  modelValue: {
+    type: [String, Number],
+    default: "",
+  },
+});
+</script>
