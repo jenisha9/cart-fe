c2efabd9a5b62e2de34c46de2fbaf67e4513fe11
jenisha9 <njenisha78@gmail.com>
2024-04-22 11:45:09 +0545

    feat: pass error to inputbox component

--- src/pages/UserLogin.vue
+++ src/pages/UserLogin.vue
@@ -4,17 +4,16 @@
       <h2 class="text-pink-500 text-2xl font-semibold mb-4">Login</h2>
       <form @submit.prevent="submitForm" method="post">
         <div class="mb-4">
-          <InputBox label="Email" v-model="email" type="email" />
+          <InputBox label="Email" v-model="email" type="email" :error="error"/>
         </div>
         <div class="mb-6">
-          <InputBox label="Password" v-model="password" type="password" />
+          <InputBox label="Password" v-model="password" type="password" :error="error"/>
         </div>
         <button type="submit"
-          class="w-full bg-pink-500 text-white font-semibold px-4 py-2 rounded hover:bg-pink-600 focus:outline-none focus:bg-pink-600">
-          Login
-        </button>
+        class="w-full bg-pink-500 text-white font-semibold px-4 py-2 rounded hover:bg-pink-600 focus:outline-none focus:bg-pink-600">
+        Login
+      </button>
       </form>
-      {{ error }}
       <div class="mt-4 text-center">
         <a href="/register" class="text-pink-500 hover:text-pink-700 font-semibold">Create an Account</a>
       </div>
@@ -27,7 +26,6 @@ import { LoginForm } from "@/composables/LoginForm.js";
 import router from "@/router/routes.js";
 import { watch } from "vue";
 import InputBox from "@/components/InputBox.vue";
-// import ErrorWarning from "@/components/ErroWarning.vue";
 
 const { email, password, submitForm, data, error } = LoginForm();
 watch(data, () => {
