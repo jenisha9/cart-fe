c820172e9aa9e1fff2443d8d485b852fb1a9c72e
jenisha9 <njenisha78@gmail.com>
2024-04-22 11:44:20 +0545

    feat: use of definemodel

--- src/components/InputBox.vue
+++ src/components/InputBox.vue
@@ -1,27 +1,35 @@
 <template>
   <div>
-    <label class="block mb-2 text-left text-gray-700 font-bold">{{
-      label
-    }}</label>
+    <label class="block mb-2 text-left text-gray-700 font-bold">{{ label }}</label>
     <input
-  :value="modelValue"
-  @input="$emit('update:modelValue', $event.target.value)"
-  v-bind="$attrs"
-  class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
-/>
+      v-model="value"
+      v-bind="$attrs"
+      class="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+    />
+    <div v-if="error && error.password" class="text-red-500">
+      {{ error.password }}
+    </div>
+    <div v-if="error && error.email" class="text-red-500">
+      {{ error.email }}
+    </div>
   </div>
-</template>
+</template>         
 
 <script setup>
-import defineProps from "vue";
+import { defineProps, defineModel } from "vue";
+const value = defineModel();
 
 defineProps({
   label: {
     type: String,
     default: "",
   },
-  modelValue: {
-    type: [String, Number],
+  error: {
+    type: Object,
+    default: () => {},
+  },
+  field: {
+    type: String,
     default: "",
   },
 });
