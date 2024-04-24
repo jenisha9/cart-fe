2150c10d601aa9bc22deab16e1e7c1a42a7e576f
jenisha9 <njenisha78@gmail.com>
2024-04-24 15:54:02 +0545

    fix: handle add to cart using pinia

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -7,7 +7,7 @@
         <img :src="item.image" :alt="item.title" class="w-full h-64 object-cover mb-4">
         <p class="text-gray-700 mb-2">{{ item.description.length > 50 ? item.description.slice(0, 50) + '...' : item.description }}</p>
         <p class="text-gray-800 font-semibold mb-2">${{ item.price.toFixed(2) }}</p>
-        <button @click="addToCart(index)" class="bg-pink-400 text-white font-semibold px-4 py-2 rounded hover:bg-pink-500 focus:outline-none focus:bg-pink-600">Add to Cart</button>
+        <button @click="addToCart(index, product)" class="bg-pink-400 text-white font-semibold px-4 py-2 rounded hover:bg-pink-500 focus:outline-none focus:bg-pink-600">Add to Cart</button>
       </div>
     </div> 
   </div>
@@ -17,8 +17,8 @@
 import axios from 'axios';
 import { onMounted, ref } from 'vue';
 import { useCart } from '@/composables/UseCart';
-
 const cartStore = useCart();
+
 const items = ref([]);
 const error = ref(null);
 
@@ -31,32 +31,10 @@ const fetchData = async () => {
   }
 };
 
-const addToCart = (index) => {
-  const selectedItem = items.value[index];
-  console.log(selectedItem);
-
-  const existingItem = items.value.find(item => item.id === selectedItem.id);
-
-  if (existingItem) {
-    existingItem.quantity++;
-  } else {
-    const token = localStorage.getItem('token');
-
-    const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Token ${token}`
-    };
-    axios.post(`http://127.0.0.1:8000/cart/${selectedItem.id}`, { headers })
-      .then(response => {
-        console.log('Item added to cart:', response.data);
-        cartStore.addToCart(selectedItem, index);
-      })
-      .catch(error => {
-        console.error('Error adding item to cart:', error);
-      });
-  }
-};
-
+const addToCart = (index,product) => {
+  console.log(product)
+   cartStore.addToCart(items.value[index],index); 
+ }
 
 onMounted(fetchData);
 </script>
