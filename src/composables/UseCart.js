74c54d83fe097358fb58e61b54dfa128b8af1f72
jenisha9 <njenisha78@gmail.com>
2024-04-25 15:55:21 +0545

    feat: remove from cart

--- src/composables/UseCart.js
+++ src/composables/UseCart.js
@@ -1,14 +1,17 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import { useFetch } from "./fetch.js";
-const data = ref(null);
 const error = ref(null);
+const get_data = ref(null);
+
 export const useCart = defineStore("counter", {
   id: "cart",
   state: () => ({
     items: [],
     total: 0,
     quantity: [],
+    get_items: [],
+    quan: 1,
   }),
   actions: {
     addToCart(product, index) {
@@ -20,15 +23,14 @@ export const useCart = defineStore("counter", {
         this.quantity[index + 1] = 1;
       }
       this.total += Number(product.price) * this.quantity[index + 1];
-      console.log(this.total);
-      console.log(this.quantity[index]);
+      this.quan = this.quantity[index + 1];
 
       try {
         const token = localStorage.getItem("token");
 
         const requestData = {
           product_id: product.id,
-          quantity: this.quantity[index],
+          quantity: this.quan,
         };
         const options = {
           method: "POST",
@@ -41,8 +43,7 @@ export const useCart = defineStore("counter", {
 
         useFetch("http://127.0.0.1:8000/cart/add-to-cart/", options)
           .then((res) => {
-            data.value = res;
-            console.log(res, "res");
+            get_data.value = res;
           })
           .catch((err) => {
             error.value = err;
@@ -52,15 +53,60 @@ export const useCart = defineStore("counter", {
         console.error("Error during fetching data:", err);
       }
     },
-    increment(product, index) {
-        console.log(index)
-      this.quantity[product.id] += 1;
-      this.total += Number(product.price),
-      console.log(this.total);
-    },
-    removeFromCart(product) {
-      this.items = this.items.filter((items) => items.id !== product.id);
-      this.total -= product.price;
-    },
+
+    removeFromCart(index,selected_item) { 
+      try {
+        const productid= selected_item.product.id
+        const token = localStorage.getItem("token");
+            const requestData = {
+          product_id: productid,
+        };
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Token " + token,
+          },
+          body: JSON.stringify(requestData),
+        };
+    
+        useFetch("http://127.0.0.1:8000/cart/remove-from-cart/", options)
+          .then((res) => {
+            console.log("Item removed from cart:", res);
+          })
+          .catch((err) => {
+            console.error("Error removing item from cart:", err);
+          });
+      } catch (err) {
+        console.error("Error during fetching data:", err);
+      }
+    }
   },
-});
+
+})
+
+export function get_all_cart() {
+  const error = ref(null);
+  const cart_items = ref(null);
+  try {
+    const token = localStorage.getItem("token");
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Token " + token,
+      },
+    };
+    useFetch("http://127.0.0.1:8000/cart/get-from-cart/", options)
+      .then((res) => {
+        cart_items.value = res;
+      })
+      .catch((err) => {
+        error.value = err;
+      });
+  } catch (err) {
+    error.value = err;
+    console.error("Error during fetching data:", err);
+  }
+  return { cart_items, error }
+}
